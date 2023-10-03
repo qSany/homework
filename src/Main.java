8d755b6ab818a7f64e777f4f1e15f213191862d1
admin-rez <kraken_1111@mail.ru>
2023-10-03 14:54:17 +0300

    Домашка сделанна

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        var dog = 8.0;
+        System.out.println(dog);
+        var cat = 3.6;
+        System.out.println(cat);
+        var paper = 763789;
+        System.out.println(paper);
+        var x = 763789;
+        var y = 4;
+        var result = x + y;
+        System.out.println("x + y = " + result);
+
+
+}
